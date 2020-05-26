41ba93f18c0eb48dc8f53dea460ac35fcb29ad85
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-05-26 11:27:47 +0100

    add front-end stuff

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def resume(request):
+    return render(request, 'resume.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def contact(request):
+    return render(request, 'contact.html')
